7f024759200728ee514ab10ad08cb94d0f84f0f2
BRUNO DAL PRÁ <bruno_pra@estudante.sesisenai.org.br>
2025-04-03 09:28:32 -0300

    objeto + pular

--- script.js
+++ script.js
@@ -1,29 +1,107 @@
-const canvas = document.getElementById('JogoCanvas')
-const ctx = canvas.getContext('2d')
+const canvas = document.getElementById('JogoCanvas');
+const ctx = canvas.getContext('2d');
+
+document.addEventListener('keypress', (e) => {
+    if (e.code === 'Space') {
+        personagem.saltar();
+    }
+});
 
 class Entidade {
-    constructor(x, y, largura, altura, cor){
+    constructor(x, y, largura, altura, cor) {
         this.x = x;
         this.y = y;
         this.largura = largura;
         this.altura = altura;
         this.cor = cor;
     }
+
     desenhar() {
-        ctx.fillStyle = this.cor
-        ctx.fillRect(this.x, this.y, this.altura, this.largura)
+        ctx.fillStyle = this.cor;
+        ctx.fillRect(this.x, this.y, this.largura, this.altura);
     }
 }
 
-class Jogo { 
-    cosntructor() {
-        this.loop = new this.loop.build()
+class Personagem extends Entidade {
+    #velocidade_y
+    constructor(x, y, largura, altura, cor) {
+        super(x, y, largura, altura, cor);
+        this.velocidade_y = 0;
+        this.pulando = false;
     }
-    loop () {
-        console.log('Em andamento...')
-        requestAnimationFrame(this.loop)
+
+    saltar() {
+        this.velocidade_y = -15;
+        this.pulando = true;
+    }
+
+    atualizar() {
+        this.y += this.velocidade_y;
+        if (this.pulando) {
+            this.velocidade_y += 1;
+            if (this.y >= canvas.height - this.altura) {
+                this.y = canvas.height - this.altura;
+                this.pulando = false;
+                this.velocidade_y = 0;
+            }
+        }
     }
 }
-const obstaculo = new Entidade(100, 100, 50, 50, 'white')
-const jogo = new Jogo()
-jogo.loop(obstaculo)
+
+class Obstaculo extends Entidade {
+    #velocidade_x
+    constructor(x, y, largura, altura, cor) {
+        super(x, y, largura, altura, cor);
+        this.velocidade_x = -5;
+    }
+
+    desenhar() {
+        ctx.fillStyle = this.cor;
+        ctx.fillRect(this.x, this.y, this.largura, this.altura);
+    }
+
+    setPosition(xP, yP) {
+        this.xP = xP
+        this.yP = yP
+
+        this.x = xP
+        this.y = yP
+    }
+
+
+    atualizar() {
+        this.x += this.velocidade_x;
+            if (this.x < -100){
+                this.setPosition(800, 250)
+            }
+        }
+    }
+
+
+
+class Jogo {
+    constructor() {
+        this.loop = this.loop.bind(this);
+    }
+
+    gravidade(){
+
+    }
+
+    loop() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        quadrado.desenhar();
+        personagem.desenhar();
+        personagem.atualizar();
+        teste.desenhar();
+        teste.atualizar();
+        requestAnimationFrame(this.loop);
+    }
+}
+
+// Create entities
+const quadrado = new Entidade(100, 100, 50, 50, 'white');
+const teste = new Obstaculo(800, 250, 70, 400, "black");
+const personagem = new Personagem(150, canvas.height - 45, 45, 45, 'blue');
+const jogo = new Jogo();
+jogo.loop();
